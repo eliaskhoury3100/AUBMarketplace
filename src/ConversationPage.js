4ae886ed00d30f52de2873327e93591696b8a8b8
Haddi234 <166283844+Haddi234@users.noreply.github.com>
2024-08-09 02:47:34 +0300

    Update ConversationPage.js

--- src/ConversationPage.js
+++ src/ConversationPage.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import './ConversationPage.css'; // Correct CSS file import
+import React, { useState, useEffect } from 'react';
+import './ConversationPage.css';
 
 const ConversationPage = () => {
-  const users = ["Lama", "Hadi", "Elias", "Tia", "Ahmad", "Miriam"];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const accessToken = localStorage.getItem('accessToken');
+      const userId = localStorage.getItem('username');
+      const userApiUrl = `https://tv6a49ucmd.execute-api.eu-north-1.amazonaws.com/default/retreivelistofusers?userId=${userId}`;
+
+      try {
+        const userResponse = await fetch(userApiUrl, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        });
+
+        if (userResponse.ok) {
+          const userData = await userResponse.json();
+          setUsers(userData); // Assuming the API returns an array of users
+          console.log("Users data fetched successfully:", userData);
+        } else {
+          throw new Error(`Failed to fetch users data: ${userResponse.status}`);
+        }
+      } catch (error) {
+        console.error('Error fetching users data:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleUserClick = (username) => {
+    window.location.href = `/messages?recipient=${username}`;
+
+  };
 
   return (
     <div className="message-page">
@@ -17,18 +51,12 @@ const ConversationPage = () => {
       </div>
       <main className="messages-list">
         {users.map((user, index) => (
-          <div className="user-item" key={index}>
+          <div className="user-item" key={index} onClick={() => handleUserClick(user)}>
             <div className="user-avatar"></div>
             <div className="user-name">{user}</div>
           </div>
         ))}
       </main>
-      <nav className="nav-bar">
-        <a href="#"><img src="images/home-icon.png" alt="Home" /><span>Home</span></a>
-        <a href="#"><img src="images/sell-icon.png" alt="Sell" /><span>Sell</span></a>
-        <a href="#" className="active"><img src="images/messages-icon.png" alt="Messages" /><span>Messages</span></a>
-        <a href="#"><img src="images/profile-icon.png" alt="Profile" /><span>Profile</span></a>
-      </nav>
     </div>
   );
 };
