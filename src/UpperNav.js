63aa31ed05f144e55f85570d24845ff59924f9c0
Haddi234 <166283844+Haddi234@users.noreply.github.com>
2024-08-09 02:37:41 +0300

    Update UpperNav.js

--- src/UpperNav.js
+++ src/UpperNav.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './UpperNav.css';
-import { useProfileImage } from './ProfileImageContext';
 
 const UpperNav = () => {
-    const { profileImage } = useProfileImage();
+    const [profileImage, setProfileImage] = useState('');
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+          const accessToken = localStorage.getItem('accessToken');
+          const userApiUrl = `https://aymj5wwai6.execute-api.eu-north-1.amazonaws.com/retreiveUserInfo?userId=${localStorage.getItem('userId')}`;
+          try {
+            const response = await fetch(userApiUrl, {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${accessToken}`
+              }
+            });
+      
+            if (response.ok) {
+              const userData = await response.json();
+              setProfileImage(userData['custom:ProfilePicture'] || '');
+              console.log("User profile fetched successfully:", userData);
+            } else {
+              throw new Error('Failed to fetch user profile');
+            }
+          } catch (error) {
+            console.error('Error fetching user profile:', error);
+          }
+        };
+      
+        fetchUserProfile();
+    
+      }, []);
 
     return (
         <nav className="upper-nav">
             <div className="nav-item">
                 <img src={'https://marketplacepictures.s3.eu-north-1.amazonaws.com/logo.png'} alt="Website Logo" className="nav-logo"/>
             </div>
-            <div className="nav-item">
-      
+            <div className="nav-item profile-container">
+                <img src={profileImage} alt="Profile" className="profile-picture" />
             </div>
         </nav>
     );
