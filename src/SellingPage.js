323bd2f67939b620670f73d65a29bb8ca7594dce
Haddi234 <166283844+Haddi234@users.noreply.github.com>
2024-08-09 15:34:22 +0300

    Updated SellingPage.js

--- src/SellingPage.js
+++ src/SellingPage.js
@@ -9,13 +9,14 @@ const CategoriesPage = () => {
   const [price, setPrice] = useState('');
   const [currency, setCurrency] = useState('USD');
   const [category, setCategory] = useState('');
+  const [size, setSize] = useState('');  // Added size state for conditional input
+  const [warranty, setWarranty] = useState(''); // State for storing the warranty option
+
   const [message, setMessage] = useState('');
   const [termsAccepted, setTermsAccepted] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   
   const postButtonRef = useRef(null);
-
-
 
   const toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -37,95 +38,96 @@ const CategoriesPage = () => {
     setSelectedFiles([...selectedFiles, ...filesWithBase64].slice(0, 6));
   };
 
-  // Assuming postButtonRef is already defined and attached to your "POST" button
-const handlePostButtonClick = () => {
-  setTimeout(() => {
-    if (postButtonRef.current) {
-      postButtonRef.current.focus();
-      postButtonRef.current.style.backgroundColor = '#3d3d3d'; // Use a darker color for visibility
-    }
-  }, 0);
-};
-
-
-
-
-  const handleSubmit = async (event) => {
-  
-  event.preventDefault();
-  setIsSubmitting(true);
-  const userId= localStorage.getItem('username'); // Retrieve the access token from local storage
-  const accessToken = localStorage.getItem('accessToken'); // Retrieve the access token from local storage
-  if (!accessToken) {
-    setMessage('You are not authenticated. Please login.');
-    setIsSubmitting(false);
-    return;
-  }
-
-  const requestBody = {
-    userId,
-    name, 
-    description, 
-    condition, 
-    currency, 
-    price, 
-    category, 
-    files: selectedFiles
+  const handlePostButtonClick = () => {
+    setTimeout(() => {
+      if (postButtonRef.current) {
+        postButtonRef.current.focus();
+        postButtonRef.current.style.backgroundColor = '#3d3d3d'; // Use a darker color for visibility
+      }
+    }, 0);
   };
 
-  try {
-    const response = await fetch('https://tuo6ab9fx3.execute-api.eu-north-1.amazonaws.com/uploadproduct', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}` // Include the access token in the Authorization header
-      },
-      body: JSON.stringify(requestBody)
-    });
-
-    const result = await response.json();
-   
-    if (response.ok) {
-      // Clear fields after successful submission
-      setName('');
-      setDescription('');
-      setCondition('');
-      setPrice('');
-      setCurrency('USD');
-      setCategory('');
-      setSelectedFiles([]);
-      setTermsAccepted(false)
-      setMessage('Product uploaded successfully!');
-    } else {
-      throw new Error(result.message || 'Failed to upload product due to server error');
-    }
-   
-    // Clear message after 5 seconds
-    setTimeout(() => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    const userId = localStorage.getItem('username');
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      setMessage('You are not authenticated. Please login.');
       setIsSubmitting(false);
-      setMessage('');
-    }, 10000);
+      return;
+    }
 
-  } catch (error) {
-    console.error('Error uploading product:', error);
-    setMessage(error.message || 'Failed to upload product');
-  }
-  finally {
-    setIsSubmitting(false);
-  }
-};
+    const requestBody = {
+      userId,
+      name, 
+      description, 
+      condition, 
+      currency, 
+      price, 
+      category,
+      size, // Include size in the request
+      warranty,
+      files: selectedFiles
+    };
 
+    try {
+      const response = await fetch('https://tuo6ab9fx3.execute-api.eu-north-1.amazonaws.com/uploadproduct', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify(requestBody)
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        setName('');
+        setDescription('');
+        setCondition('');
+        setPrice('');
+        setCurrency('USD');
+        setCategory('');
+        setSize(''); // Reset size
+        setSelectedFiles([]);
+        setTermsAccepted(false);
+        setMessage('Product uploaded successfully!');
+      } else {
+        throw new Error(result.message || 'Failed to upload product due to server error');
+      }
+      
+      setTimeout(() => {
+        setIsSubmitting(false);
+        setMessage('');
+      }, 10000);
+    } catch (error) {
+      console.error('Error uploading product:', error);
+      setMessage(error.message || 'Failed to upload product');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    if (!["Clothing for Men", "Clothing for Women", "Sports Wear for Men", "Sports Wear for Women"].includes(e.target.value)) {
+      setSize('');
+    }
+    if (!["Mobile Phones & Accessories", "Laptops & Tablets", "Computers & Computer Parts", "Gaming Consoles & Video Games", "TV", "Speakers", "Cameras"].includes(e.target.value)) {
+      setWarranty('');
+    }
+    
+  };
 
   const renderImagePreviews = () => (
     <div className="image-previews-container">
-      {selectedFiles.length > 0 ? (
-        selectedFiles.map((file, index) => (
-          <div key={index} className="image-preview">
-            <img src={`data:image/jpeg;base64,${file.content}`} alt={`preview ${index}`} />
-            <button onClick={() => setSelectedFiles(selectedFiles.filter((_, i) => i !== index))} className="remove-image">×</button>
-          </div>
-        ))
-      ) : (
+      {selectedFiles.length > 0 ? selectedFiles.map((file, index) => (
+        <div key={index} className="image-preview">
+          <img src={`data:image/jpeg;base64,${file.content}`} alt={`preview ${index}`} />
+          <button onClick={() => setSelectedFiles(selectedFiles.filter((_, i) => i !== index))} className="remove-image">×</button>
+        </div>
+      )) : (
         <p className="no-images-text">No images added yet.</p>
       )}
     </div>
@@ -133,9 +135,7 @@ const handlePostButtonClick = () => {
 
   return (
     <div className="selling">
-      <div className="header">
-        
-      </div>
+      <div className="header"></div>
       
       <form onSubmit={handleSubmit}>
         <div className="form-group">
@@ -144,46 +144,69 @@ const handlePostButtonClick = () => {
         </div>
         <div className="form-group">
           <label htmlFor="category">Category</label>
-          <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)} required>
+          <select id="category" name="category" value={category} onChange={handleCategoryChange} required>
             <option value="">Select a category</option>
-            
             <optgroup label="Electronics">
-              <option value="mobile_phones_accessories">Mobile Phones & Accessories</option>
-              <option value="laptops_tablets">Laptops & Tablets</option>
-              <option value="computers_parts">Computers & Computer Parts</option>
-              <option value="gaming_consoles_games">Gaming Consoles & Video Games</option>
-              <option value="tv">TV</option>
-              <option value="speakers">Speakers</option>
-              <option value="cameras">Cameras</option>
-            </optgroup>
+            <option value="Mobile Phones & Accessories">Mobile Phones & Accessories</option>
+            <option value="Laptops & Tablets">Laptops & Tablets</option>
+            <option value="Computers & Computer Parts">Computers & Computer Parts</option>
+            <option value="Gaming Consoles & Video Games">Gaming Consoles & Video Games</option>
+            <option value="TV">TV</option>
+            <option value="Speakers">Speakers</option>
+            <option value="Cameras">Cameras</option>
+          </optgroup>
 
-            <optgroup label="Fashion">
-              <option value="clothing_men">Clothing for Men</option>
-              <option value="accessories_men">Accessories for Men</option>
-              <option value="clothing_women">Clothing for Women</option>
-              <option value="accessories_women">Accessories for Women</option>
-              <option value="makeup_cosmetics">Makeup and Cosmetics</option>
-              <option value="jewelry">Jewelry</option>
-              <option value="watches">Watches</option>
-              <option value="shoes">Shoes</option>
-            </optgroup>
+          <optgroup label="Fashion">
+            <option value="Clothing for Men">Clothing for Men</option>
+            <option value="Accessories for Men">Accessories for Men</option>
+            <option value="Clothing for Women">Clothing for Women</option>
+            <option value="Accessories for Women">Accessories for Women</option>
+            <option value="Makeup and Cosmetics">Makeup and Cosmetics</option>
+            <option value="Jewelry">Jewelry</option>
+            <option value="Watches">Watches</option>
+            <option value="Shoes">Shoes</option>
+          </optgroup>
 
-            <optgroup label="Sports & Outdoors">
-              <option value="sports_wear_men">Sports Wear for Men</option>
-              <option value="sports_wear_women">Sports Wear for Women</option>
-              <option value="sports_equipment">Sports Equipment</option>
-            </optgroup>
+          <optgroup label="Sports & Outdoors">
+            <option value="Sports Wear for Men">Sports Wear for Men</option>
+            <option value="Sports Wear for Women">Sports Wear for Women</option>
+            <option value="Sports Equipment">Sports Equipment</option>
+          </optgroup>
 
-            <optgroup label="Entertainment & Hobbies">
-              <option value="musical_instruments_gear">Musical Instruments & Gear</option>
-              <option value="books">Books</option>
-              <option value="music_movies">Music & Movies</option>
-              <option value="stationery">Stationery</option>
-            </optgroup>
-
+          <optgroup label="Entertainment & Hobbies">
+            <option value="Musical Instruments & Gear">Musical Instruments & Gear</option>
+            <option value="Books">Books</option>
+            <option value="Music & Movies">Music & Movies</option>
+            <option value="Stationery">Stationery</option>
+          </optgroup>
           </select>
-
         </div>
+
+        {/* Conditionally render the size select input if the category is one of the clothing or sports wear options */}
+        {['Clothing for Men', 'Clothing for Women', 'Sports Wear for Men', 'Sports Wear for Women'].includes(category) && (
+          <div className="form-group">
+            <label htmlFor="size">Size</label>
+            <select id="size" name="size" value={size} onChange={(e) => setSize(e.target.value)} required>
+              <option value="">Select a size</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+            </select>
+          </div>
+        )}
+
+        {["Mobile Phones & Accessories", "Laptops & Tablets", "Computers & Computer Parts", "Gaming Consoles & Video Games", "TV", "Speakers", "Cameras"].includes(category) && (
+          <div className="form-group">
+            <label htmlFor="warranty">Warranty</label>
+            <select id="warranty" name="warranty" value={warranty} onChange={(e) => setWarranty(e.target.value)} required>
+              <option value="">Select warranty option</option>
+              <option value="Warranty">Warranty</option>
+              <option value="No Warranty">No Warranty</option>
+            </select>
+          </div>
+        )}
+
         <div className="form-group">
           <label htmlFor="description">Description</label>
           <textarea id="description" name="description" placeholder="Add a description" rows="4" maxLength="500" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
@@ -230,17 +253,13 @@ const handlePostButtonClick = () => {
           >
             {isSubmitting ? 'Posting...' : 'POST'}
           </button>
-
-
-       
-
-      </form>
-      {message && (
+        {message && (
             <p className={message.includes('successfully') ? 'message-success' : 'message-error'}>
                 {message}
-            </p>)}
+            </p>
+        )}
+      </form>
     </div>
-    
   );
 };
 
