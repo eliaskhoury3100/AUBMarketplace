c77c6c1c7504d1b7b9ae9eb9770f382c3f664185
Haddi234 <166283844+Haddi234@users.noreply.github.com>
2024-08-10 03:48:57 +0300

    Updated Product.js

--- src/Product.js
+++ src/Product.js
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './Product.css'; // Ensure the CSS file path is correct
 
+
 const ProductDetail = () => {
-    const { id } = useParams();
+    const { id, pk } = useParams();
     const [product, setProduct] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const [activeIndex, setActiveIndex] = useState(0);
-
+    const [profilePicture, setProfilePicture] = useState(null);
+    const [username, setusername]= useState('');
+    const decodedId = decodeURIComponent(id);
+    const decodedPk = decodeURIComponent(pk);
+    const encodedId = encodeURIComponent(id);  // This will convert '#' to '%23'
+    const encodedPk = encodeURIComponent(pk);
     useEffect(() => {
         const fetchProductDetails = async () => {
             setIsLoading(true);
             const accessToken = localStorage.getItem('accessToken');
             try {
-                const response = await fetch(`https://1hdka2qnw7.execute-api.eu-north-1.amazonaws.com/default/productdetail?id=${id}`, {
+                const response = await fetch(`https://1hdka2qnw7.execute-api.eu-north-1.amazonaws.com/default/productdetail?id=${encodedId}&pk=${encodedPk}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
@@ -26,16 +32,39 @@ const ProductDetail = () => {
                 const data = await response.json();
 
                 setProduct(data);  
+                const user= data[0].UserID;
                 console.log(data)
+                const userApiUrl = `https://aymj5wwai6.execute-api.eu-north-1.amazonaws.com/retreiveUserInfo?userId=${user}`;
+                const userResponse = await fetch(userApiUrl, {
+                    method: 'GET',
+                    headers: {
+                         'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                });
+                if (!userResponse.ok) throw new Error('User not found.');
+
+                const userData = await userResponse.json();
+                console.log("UserData:", userData)
+                setProfilePicture(userData['custom:ProfilePicture'] || ''); // Assuming the API returns a profilePictureUrl
+                setusername(userData['email'])
+        
+                console.log(profilePicture)
+                console.log(username)
             } catch (err) {
                 setError(err.message);
             } finally {
                 setIsLoading(false);
             }
+           
         };
 
         fetchProductDetails();
-    }, [id]); // Fetch product details whenever `id` changes
+    }, [id, pk]); // Fetch product details whenever `id` changes
+
+    const extractUsername = (email) => {
+        return email ? email.split('@')[0] : '';
+    };
 
     const ProductCard = ({ product }) => {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -45,40 +74,42 @@ const ProductDetail = () => {
     };
 
     const handleMessageSellerClick = () => {
-        const nameParam = encodeURIComponent(product[0].Name);
+        const nameParam = encodeURIComponent(product[0].Title);
         const priceParam = encodeURIComponent(product[0].Price);
-        const imageParam = encodeURIComponent(product[0].ImageUrls[0]);
-        const userID= encodeURIComponent(product[0].UserID);
-        const productID= encodeURIComponent(product[0].ProductID);
+        const imageParam = encodeURIComponent(product[0].ImageUrl[0]);
+        const userID= encodeURIComponent(extractUsername(username));
+        const productID= encodeURIComponent(product[0].SK);
         window.location.href = `/messages?name=${nameParam}&price=${priceParam}&image=${imageParam}&sellerID=${userID}&productID=${productID}`;
     };
 
-    if (product.length === 0 || !product[0].ImageUrls || product[0].ImageUrls.length === 0) {
+    if (product.length === 0 || !product[0].ImageUrl || product[0].ImageUrl.length === 0) {
         return <p>No product or images available.</p>;
     }
 
-    if (product.length === 0 || !product[0].ImageUrls || product[0].ImageUrls.length === 0) {
+    if (product.length === 0 || !product[0].ImageUrl || product[0].ImageUrl.length === 0) {
         return <p>No product or images available.</p>;
     }
 
     return (
         <div className="full-page-container">
             <div className="upper-section">
-                
+            {profilePicture && <img src={profilePicture} alt="Profile Picture" className="profile-picture" />}
+                    {username && <p className="username">{extractUsername(username)}</p>}
             </div>
             <div className="imageecarousel">
                 
-                    <img src={product[0].ImageUrls[activeIndex]} alt={`Product ${product[0].Name} Image`} className="producttimage" />
+                    <img src={product[0].ImageUrl[activeIndex]} alt={`Product ${product[0].Title} Image`} className="producttimage" />
                     <div className="carousel-dots">
-                        {product[0].ImageUrls.map((_, index) => (
+                        {product[0].ImageUrl.map((_, index) => (
                             <span key={index} className={`dot ${index === activeIndex ? 'active' : ''}`} onClick={() => handleDotClick(index)}></span>
                         ))}
                     </div>
                 
             </div>
             <div className="producttinfo">
-                <h3>{product[0].Name}</h3>
+                <h3>{product[0].Title}</h3>
                 <p>Description: {product[0].Description}</p>
+                <p>Size: {product[0].Size}</p>
                 <p>Condition: {product[0].Condition}</p>
                 <p>Price: ${product[0].Price}</p>
                 <button className="message-seller-btn" onClick={handleMessageSellerClick}>Message Seller</button>
